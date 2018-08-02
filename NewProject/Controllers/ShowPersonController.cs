42d2710a18f595264bcde092c9ebf6c7c79e6566
sudhir <sudhir@ICAN-23>
2018-08-02 11:00:31 +0530

    show person list

---
+++ NewProject/Controllers/ShowPersonController.cs
@@ -0,0 +1,23 @@
+﻿using NewProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewProject.Controllers
+{
+    public class ShowPersonController : Controller
+    {
+        // GET: ShowPerson
+        public ActionResult Index()
+        {
+            List<First> persons = new List<First>()
+    {
+        new First { FirstName = "Sherlock", LastName = "Holmes" },
+        new First { FirstName = "James", LastName = "Watson" }
+    };
+            return View(persons);
+        }
+    }
+}
---
+++ NewProject/Models/First.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewProject.Models
+{
+    public class First
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
